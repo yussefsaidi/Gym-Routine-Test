c2a8e6f658bcf15da8d98cbe8f36bf2f7cfc3822
Yussef <saidi.yussef@live.fr>
2020-02-05 14:12:35 -0500

    Test RecyclerView display with fake data

--- app/src/main/java/com/yussefsaidi/ppl/ExercisesListActivity.java
+++ app/src/main/java/com/yussefsaidi/ppl/ExercisesListActivity.java
@@ -1,21 +1,53 @@
 package com.yussefsaidi.ppl;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.widget.LinearLayout;
 
+import com.yussefsaidi.ppl.adapters.ExerciseRecyclerAdapter;
 import com.yussefsaidi.ppl.models.Exercise;
+
+import java.util.ArrayList;
 
 public class ExercisesListActivity extends AppCompatActivity {
 
     private static final String TAG = "ExercisesListActivity";
 
+    // UI Components
+    private RecyclerView mRecyclerView;
+
+    // Vars
+    private ArrayList<Exercise> mExercises = new ArrayList<>();
+    private ExerciseRecyclerAdapter mExerciseRecyclerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_exercise_list);
 
-        Exercise exercise = new Exercise();
+        mRecyclerView = findViewById(R.id.recyclerView);
+        initRecyclerView();
+        insertFakeExercises();
+    }
+
+    private void insertFakeExercises(){
+        for(int i = 0; i < 100; i++){
+            Exercise exercise = new Exercise();
+            exercise.setName("Exercise # " + i);
+            exercise.setSets("Sets # " + i);
+            exercise.setRepetitions("Reps # " + i);
+            mExercises.add(exercise);
+        }
+        mExerciseRecyclerAdapter.notifyDataSetChanged();
+    }
+
+    private void initRecyclerView(){
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mExerciseRecyclerAdapter = new ExerciseRecyclerAdapter(mExercises);
+        mRecyclerView.setAdapter(mExerciseRecyclerAdapter);
     }
 }
