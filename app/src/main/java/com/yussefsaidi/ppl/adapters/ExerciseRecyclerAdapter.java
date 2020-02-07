4b49519668b235e4f8c7f2341ca79609502a7e45
Yussef <saidi.yussef@live.fr>
2020-02-07 12:44:34 -0500

    Create onClick events for editing

--- app/src/main/java/com/yussefsaidi/ppl/adapters/ExerciseRecyclerAdapter.java
+++ app/src/main/java/com/yussefsaidi/ppl/adapters/ExerciseRecyclerAdapter.java
@@ -1,10 +1,8 @@
 package com.yussefsaidi.ppl.adapters;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,33 +29,16 @@ public class ExerciseRecyclerAdapter extends RecyclerView.Adapter {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_exercise_list_item, parent, false);
         final ExerciseViewHolder holder = new ExerciseViewHolder(view);
 
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: position: " + holder.getAdapterPosition());
-                mExercise = mExercises.get(holder.getAdapterPosition());
-                boolean expanded = mExercise.isExpanded();
-                if(expanded){
-                    mExercise.setExpanded(false);
-                }
-                else{
-                    mExercise.setExpanded(true);
-                }
-                notifyItemChanged(holder.getAdapterPosition());
-            }
-        });
         return holder;
     }
 
-   /**
-    }**/
-
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
-        ((ExerciseViewHolder)holder).name.setText(mExercises.get(position).getName());
+        ((ExerciseViewHolder)holder).mViewName.setText(mExercises.get(position).getName());
+        ((ExerciseViewHolder)holder).mEditName.setText(mExercises.get(position).getName());
         ((ExerciseViewHolder)holder).repetitions.setText(mExercises.get(position).getRepetitions());
         ((ExerciseViewHolder)holder).sets.setText(mExercises.get(position).getSets());
-        ((ExerciseViewHolder)holder).bind(mExercises.get(position));
+        //((ExerciseViewHolder)holder).bind(mExercises.get(position));
 
     }
 
--- app/src/main/java/com/yussefsaidi/ppl/adapters/ExerciseViewHolder.java
+++ app/src/main/java/com/yussefsaidi/ppl/adapters/ExerciseViewHolder.java
@@ -1,6 +1,13 @@
 package com.yussefsaidi.ppl.adapters;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -10,18 +17,36 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.yussefsaidi.ppl.R;
 import com.yussefsaidi.ppl.models.Exercise;
 
-public class ExerciseViewHolder extends RecyclerView.ViewHolder {
+public class ExerciseViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-    TextView name, sets, repetitions;
+    private static final int EDIT_MODE_DISABLED = 0;
+    private static final int EDIT_MODE_ENABLED = 1;
+
+    TextView sets, repetitions;
     private LinearLayout mSubItem;
+    private Button mEditButton;
+    private ImageButton mCheckContainer;
+    EditText mEditName;
+    TextView mViewName;
+    View exerciseItem;
+    private int mMode;
+    Activity activity;
 
 
     public ExerciseViewHolder(@NonNull View itemView) {
         super(itemView);
-        name = itemView.findViewById(R.id.exercise_name);
+        mViewName = itemView.findViewById(R.id.exercise_text_name);
+        mEditName = itemView.findViewById(R.id.exercise_edit_name);
         sets = itemView.findViewById(R.id.exercise_reps);
         repetitions = itemView.findViewById(R.id.exercise_sets);
         mSubItem = itemView.findViewById(R.id.exercise_subinfo);
+        mEditButton = itemView.findViewById(R.id.edit_name_button);
+        mCheckContainer = itemView.findViewById(R.id.check_container);
+        exerciseItem = itemView.findViewById(R.id.exercise_item);
+        exerciseItem.setOnClickListener(this);
+        mEditButton.setOnClickListener(this);
+        mCheckContainer.setOnClickListener(this);
+
     }
 
 
@@ -33,7 +58,74 @@ public class ExerciseViewHolder extends RecyclerView.ViewHolder {
         } else {
             mSubItem.setVisibility(View.GONE);
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        activity = getActivity(view);
+        if(view.getId() == exerciseItem.getId()){
+            if(mSubItem.getVisibility() == View.VISIBLE){
+                mSubItem.setVisibility(View.GONE);
+            } else
+                mSubItem.setVisibility(View.VISIBLE);
+        }
+
+        if (view.getId() == mEditButton.getId()) {
+            enableEditMode(activity);
+        }
+        // Check pressed in edit mode
+        if (view.getId() == mCheckContainer.getId()) {
+            disableEditMode(activity);
+        }
+        // When edit mode is enabled, clicking anywhere results in disabling edit mode
 
     }
+
+
+    private void enableEditMode(Activity activity) {
+        mViewName.setVisibility(View.GONE);
+        mEditName.setVisibility(View.VISIBLE);
+        mMode = EDIT_MODE_ENABLED;
+        mCheckContainer.setVisibility(View.VISIBLE);
+        mEditButton.setVisibility(View.GONE);
+        mEditName.requestFocus();
+    }
+
+    private void disableEditMode(Activity activity) {
+        mEditName.setVisibility(View.GONE);
+        mViewName.setVisibility(View.VISIBLE);
+        mMode = EDIT_MODE_DISABLED;
+        mCheckContainer.setVisibility(View.GONE);
+        mEditButton.setVisibility(View.VISIBLE);
+        hideKeyboard(activity);
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
+
+    private Activity getActivity(View view) {
+        Context context = view.getContext();
+        while (context instanceof ContextWrapper) {
+            if (context instanceof Activity) {
+                return (Activity) context;
+            }
+            context = ((ContextWrapper) context).getBaseContext();
+        }
+        return null;
+    }
+
+    private void onBackPressed(Activity activity){
+        if(mMode == EDIT_MODE_ENABLED){
+            onClick(mCheckContainer);
+        }
+        else {
+            activity.onBackPressed();
+        }
+    }
+
 
 }
