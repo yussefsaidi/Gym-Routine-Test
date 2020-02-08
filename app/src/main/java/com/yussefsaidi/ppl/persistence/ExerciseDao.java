f221a039bb059b22dd4e8f7c4df5f50dd0219c98
Yussef <saidi.yussef@live.fr>
2020-02-08 14:46:31 -0500

    Setup Dao

---
+++ app/src/main/java/com/yussefsaidi/ppl/persistence/ExerciseDao.java
@@ -0,0 +1,28 @@
+package com.yussefsaidi.ppl.persistence;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.yussefsaidi.ppl.models.Exercise;
+
+import java.util.List;
+
+@Dao
+public interface ExerciseDao {
+
+    @Insert
+    long insertExercises(Exercise... exercises);
+
+    @Query("SELECT * FROM exercises")
+    LiveData<List<Exercise>> getExercises();
+
+    @Delete
+    int delete(Exercise... exercises);
+
+    @Update
+    int update(Exercise... exercises);
+}
